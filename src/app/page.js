65ed34915cdb4e602208bfe208e9d2bf75c3c470
Mu-Solomon <musolomoni@gmail.com>
2024-02-09 13:39:18 +0300

    Light mode

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,22 @@
+"use client";
+
 import HomePage from "@/components/homePage";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Home() {
-  return <HomePage />;
+  const [theme, setTheme] = useState("light");
+  const changeTheme = () => {
+    if (theme == "dark") {
+      setTheme("light");
+    } else {
+      setTheme("dark");
+    }
+    console.log(theme);
+  };
+  return (
+    <body className={` ${theme == "dark" ? "bg-[#25273c]" : "bg-[#fafafa]"}`}>
+      <HomePage theme={theme} changeTheme={changeTheme} />
+    </body>
+  );
 }
--- src/components/homePage.jsx
+++ src/components/homePage.jsx
@@ -3,17 +3,7 @@
 import React, { useState } from "react";
 import Image from "next/image";
 
-const HomePage = () => {
-  const [theme, setTheme] = useState("light");
-
-  const changeTheme = () => {
-    if (theme == "dark") {
-      setTheme("light");
-    } else {
-      setTheme("dark");
-    }
-    console.log(theme);
-  };
+const HomePage = ({ theme, changeTheme }) => {
   return (
     <div
       id="container "
@@ -45,7 +35,7 @@ const HomePage = () => {
       >
         <span
           className={`min-w-6 h-6 border-2 border-[#393a4c] ${
-            theme == "dark" ? "border-2" : "border border-[#d2d3db]"
+            theme == "dark" ? "border-2" : "border-[1px] border-[#d2d3db]"
           } rounded-full ml-5 `}
         ></span>
         <input
@@ -54,25 +44,35 @@ const HomePage = () => {
           placeholder="Create a new todo..."
           className={` ${
             theme == "dark" ? "bg-[#25273c]" : "bg-[#fafafa]"
-          } py-3 sm:py-5 text-[#c4d4e8] w-full focus:border-transparent focus:outline-none`}
+          } py-3 sm:py-5 text-[#484b6a] w-full focus:border-transparent focus:outline-none`}
         />
       </div>
       <div id="todos" className="sm:w-[550px] sm:mt-5">
-        <ul>
+        <ul className="">
           <li className="group">
             <div
               className={`flex items-center justify-left ${
-                theme == "dark" ? "bg-[#25273c]" : "bg-[#fafafa]"
-              } bg-[#25273c] gap-3 mx-6 sm:mx-0 rounded-sm py-4  sm:py-5 border-b border-b-[#4d5066] `}
+                theme == "dark"
+                  ? "bg-[#25273c] border-b border-b-[#4d5066]"
+                  : "bg-[#fafafa] border-b border-b-[#d2d3db] "
+              } bg-[#25273c] gap-3 mx-6 sm:mx-0 rounded-sm py-4  sm:py-5 `}
             >
-              <span className="w-6 h-6 border-2 border-[#393a4c] rounded-full ml-5  bg-gradient-to-br from-[#57ddff] to-[#c058f3] hover:cursor-pointer"></span>
+              <span
+                className={`w-6 h-6 border-2  ${
+                  theme == "dark" ? "border-[#393a4c]" : "border-none"
+                } rounded-full ml-5  bg-gradient-to-br from-[#57ddff] to-[#c058f3] hover:cursor-pointer`}
+              ></span>
               <Image
                 src="/images/icon-check.svg"
                 width={11}
                 height={11}
                 className="absolute left-[50px] sm:left-[-30px] sm:relative"
               />
-              <p className="text-sm text-[#4d5066] line-through sm:ml-[-12px] hover:cursor-pointer">
+              <p
+                className={`text-sm  ${
+                  theme == "dark" ? "text-[#4d5066]" : "text-[#d2d3db]"
+                } line-through sm:ml-[-12px] hover:cursor-pointer`}
+              >
                 Complete online Javascript course
               </p>
               <Image
@@ -86,11 +86,29 @@ const HomePage = () => {
 
           {/* uncompleted */}
 
-          <li class="justify-left group mx-6 flex items-center gap-3 rounded-sm border-b border-b-[#4d5066] bg-[#25273c] py-4 sm:mx-0 sm:py-5">
-            <div class="gradient ml-5 flex h-6 w-6 items-center justify-center rounded-full bg-[#393a4c] from-[#57ddff] to-[#c058f3] hover:bg-gradient-to-br">
-              <span class="inner-gradient h-[22px] w-[22px] cursor-pointer rounded-full bg-[#25273c]"></span>
+          <li
+            class={`justify-left group mx-6 flex items-center gap-3 rounded-sm  ${
+              theme == "dark"
+                ? "bg-[#25273c] border-b border-b-[#4d5066]"
+                : "bg-[#fafafa] border-b-[1px] border-b-[#d2d3db]"
+            } py-4 sm:mx-0 sm:py-5`}
+          >
+            <div
+              class={`gradient ml-5 flex h-6 w-6 items-center justify-center rounded-full ${
+                theme == "dark" ? "bg-[#393a4c]" : "bg-[#d2d3db]"
+              }  from-[#57ddff] to-[#c058f3] hover:bg-gradient-to-br`}
+            >
+              <span
+                class={`inner-gradient h-[22px] w-[22px] cursor-pointer rounded-full ${
+                  theme == "dark" ? "bg-[#25273c]" : "bg-[#fafafa]"
+                }`}
+              ></span>
             </div>
-            <p class="text-sm text-[#c4d4e8] hover:cursor-pointer hover:text-[#c4d4e8] sm:w-[222px] sm:pl-3">
+            <p
+              class={`text-sm text-[#484b6a] hover:cursor-pointer hover:text-[#484b6a] sm:w-[222px] sm:pl-3 ${
+                theme == "dark" ? "text-[#484b6a]" : "text-[#484b6a]"
+              }`}
+            >
               Jog around the park 3x
             </p>
             <Image
@@ -100,12 +118,29 @@ const HomePage = () => {
               class="absolute right-10 sm:hidden hover:cursor-pointer group-hover:block sm:relative sm:right-[-210px]"
             />
           </li>
-
-          <li class="justify-left group mx-6 flex items-center gap-3 rounded-sm border-b border-b-[#4d5066] bg-[#25273c] py-4 sm:mx-0 sm:py-5">
-            <div class="gradient ml-5 flex h-6 w-6 items-center justify-center rounded-full bg-[#393a4c] from-[#57ddff] to-[#c058f3] hover:bg-gradient-to-br">
-              <span class="inner-gradient h-[22px] w-[22px] cursor-pointer rounded-full bg-[#25273c]"></span>
+          <li
+            class={`justify-left group mx-6 flex items-center gap-3 rounded-sm  ${
+              theme == "dark"
+                ? "bg-[#25273c] border-b border-b-[#4d5066]"
+                : "bg-[#fafafa] border-b border-b-[#d2d3db]"
+            } py-4 sm:mx-0 sm:py-5`}
+          >
+            <div
+              class={`gradient ml-5 flex h-6 w-6 items-center justify-center rounded-full ${
+                theme == "dark" ? "bg-[#393a4c]" : "bg-[#d2d3db]"
+              }  from-[#57ddff] to-[#c058f3] hover:bg-gradient-to-br`}
+            >
+              <span
+                class={`inner-gradient h-[22px] w-[22px] cursor-pointer rounded-full ${
+                  theme == "dark" ? "bg-[#25273c]" : "bg-[#fafafa]"
+                }`}
+              ></span>
             </div>
-            <p class="text-sm text-[#c4d4e8] hover:cursor-pointer hover:text-[#c4d4e8] sm:w-[222px] sm:pl-3">
+            <p
+              class={`text-sm text-[#484b6a] hover:cursor-pointer hover:text-[#484b6a] sm:w-[222px] sm:pl-3 ${
+                theme == "dark" ? "text-[#484b6a]" : "text-[#484b6a]"
+              }`}
+            >
               Jog around the park 3x
             </p>
             <Image
@@ -115,11 +150,29 @@ const HomePage = () => {
               class="absolute right-10 sm:hidden hover:cursor-pointer group-hover:block sm:relative sm:right-[-210px]"
             />
           </li>
-          <li class="justify-left group mx-6 flex items-center gap-3 rounded-sm border-b border-b-[#4d5066] bg-[#25273c] py-4 sm:mx-0 sm:py-5">
-            <div class="gradient ml-5 flex h-6 w-6 items-center justify-center rounded-full bg-[#393a4c] from-[#57ddff] to-[#c058f3] hover:bg-gradient-to-br">
-              <span class="inner-gradient h-[22px] w-[22px] cursor-pointer rounded-full bg-[#25273c]"></span>
+          <li
+            class={`justify-left group mx-6 flex items-center gap-3 rounded-sm  ${
+              theme == "dark"
+                ? "bg-[#25273c] border-b border-b-[#4d5066]"
+                : "bg-[#fafafa] border-b border-b-[#d2d3db]"
+            } py-4 sm:mx-0 sm:py-5`}
+          >
+            <div
+              class={`gradient ml-5 flex h-6 w-6 items-center justify-center rounded-full ${
+                theme == "dark" ? "bg-[#393a4c]" : "bg-[#d2d3db]"
+              }  from-[#57ddff] to-[#c058f3] hover:bg-gradient-to-br`}
+            >
+              <span
+                class={`inner-gradient h-[22px] w-[22px] cursor-pointer rounded-full ${
+                  theme == "dark" ? "bg-[#25273c]" : "bg-[#fafafa]"
+                }`}
+              ></span>
             </div>
-            <p class="text-sm text-[#c4d4e8] hover:cursor-pointer hover:text-[#c4d4e8] sm:w-[222px] sm:pl-3">
+            <p
+              class={`text-sm text-[#484b6a] hover:cursor-pointer hover:text-[#484b6a] sm:w-[222px] sm:pl-3 ${
+                theme == "dark" ? "text-[#484b6a]" : "text-[#484b6a]"
+              }`}
+            >
               Jog around the park 3x
             </p>
             <Image
@@ -129,11 +182,29 @@ const HomePage = () => {
               class="absolute right-10 sm:hidden hover:cursor-pointer group-hover:block sm:relative sm:right-[-210px]"
             />
           </li>
-          <li class="justify-left group mx-6 flex items-center gap-3 rounded-sm border-b border-b-[#4d5066] bg-[#25273c] py-4 sm:mx-0 sm:py-5">
-            <div class="gradient ml-5 flex h-6 w-6 items-center justify-center rounded-full bg-[#393a4c] from-[#57ddff] to-[#c058f3] hover:bg-gradient-to-br">
-              <span class="inner-gradient h-[22px] w-[22px] cursor-pointer rounded-full bg-[#25273c]"></span>
+          <li
+            class={`justify-left group mx-6 flex items-center gap-3 rounded-sm  ${
+              theme == "dark"
+                ? "bg-[#25273c] border-b border-b-[#4d5066]"
+                : "bg-[#fafafa] border-b border-b-[#d2d3db]"
+            } py-4 sm:mx-0 sm:py-5`}
+          >
+            <div
+              class={`gradient ml-5 flex h-6 w-6 items-center justify-center rounded-full ${
+                theme == "dark" ? "bg-[#393a4c]" : "bg-[#d2d3db]"
+              }  from-[#57ddff] to-[#c058f3] hover:bg-gradient-to-br`}
+            >
+              <span
+                class={`inner-gradient h-[22px] w-[22px] cursor-pointer rounded-full ${
+                  theme == "dark" ? "bg-[#25273c]" : "bg-[#fafafa]"
+                }`}
+              ></span>
             </div>
-            <p class="text-sm text-[#c4d4e8] hover:cursor-pointer hover:text-[#c4d4e8] sm:w-[222px] sm:pl-3">
+            <p
+              class={`text-sm text-[#484b6a] hover:cursor-pointer hover:text-[#484b6a] sm:w-[222px] sm:pl-3 ${
+                theme == "dark" ? "text-[#484b6a]" : "text-[#484b6a]"
+              }`}
+            >
               Jog around the park 3x
             </p>
             <Image
@@ -143,11 +214,29 @@ const HomePage = () => {
               class="absolute right-10 sm:hidden hover:cursor-pointer group-hover:block sm:relative sm:right-[-210px]"
             />
           </li>
-          <li class="justify-left group mx-6 flex items-center gap-3 rounded-sm border-b border-b-[#4d5066] bg-[#25273c] py-4 sm:mx-0 sm:py-5">
-            <div class="gradient ml-5 flex h-6 w-6 items-center justify-center rounded-full bg-[#393a4c] from-[#57ddff] to-[#c058f3] hover:bg-gradient-to-br">
-              <span class="inner-gradient h-[22px] w-[22px] cursor-pointer rounded-full bg-[#25273c]"></span>
+          <li
+            class={`justify-left group mx-6 flex items-center gap-3 rounded-sm  ${
+              theme == "dark"
+                ? "bg-[#25273c] border-b border-b-[#4d5066]"
+                : "bg-[#fafafa] border-b border-b-[#d2d3db]"
+            } py-4 sm:mx-0 sm:py-5`}
+          >
+            <div
+              class={`gradient ml-5 flex h-6 w-6 items-center justify-center rounded-full ${
+                theme == "dark" ? "bg-[#393a4c]" : "bg-[#d2d3db]"
+              }  from-[#57ddff] to-[#c058f3] hover:bg-gradient-to-br`}
+            >
+              <span
+                class={`inner-gradient h-[22px] w-[22px] cursor-pointer rounded-full ${
+                  theme == "dark" ? "bg-[#25273c]" : "bg-[#fafafa]"
+                }`}
+              ></span>
             </div>
-            <p class="text-sm text-[#c4d4e8] hover:cursor-pointer hover:text-[#c4d4e8] sm:w-[222px] sm:pl-3">
+            <p
+              class={`text-sm text-[#484b6a] hover:cursor-pointer hover:text-[#484b6a] sm:w-[222px] sm:pl-3 ${
+                theme == "dark" ? "text-[#484b6a]" : "text-[#484b6a]"
+              }`}
+            >
               Jog around the park 3x
             </p>
             <Image
@@ -159,36 +248,72 @@ const HomePage = () => {
           </li>
 
           <li>
-            <div className="flex items-center justify-between bg-[#25273c] gap-3 mx-6 sm:mx-0 rounded-sm py-4 sm:py-5 px-5 sm:border-b sm:border-b-[#4d5066] sm:hidden">
-              <p className="text-sm text-[#4d5066]">5 items left</p>
-              <p className="text-sm text-[#4d5066]">Clear completed</p>
+            <div
+              className={`flex items-center justify-between ${
+                theme == "dark" ? "bg-[#25273c]" : "bg-[#fafafa]"
+              } gap-3 mx-6 sm:mx-0 rounded-sm py-4 sm:py-5 px-5 sm:border-b sm:border-b-[#4d5066] sm:hidden`}
+            >
+              <p
+                className={`text-sm ${
+                  theme == "dark" ? "text-[#4d5066]" : "text-[#9394a5]"
+                }`}
+              >
+                5 items left
+              </p>
+              <p
+                className={`text-sm ${
+                  theme == "dark" ? "text-[#4d5066]" : "text-[#9394a5]"
+                }`}
+              >
+                Clear completed
+              </p>
             </div>
           </li>
         </ul>
         <div id="sort" className="mt-5  sm:mt-0">
-          <div className="flex items-center justify-center sm:justify-between bg-[#25273c] gap-3 mx-6 sm:mx-0 rounded-sm py-4 sm:py-5 px-5">
+          <div
+            className={`flex items-center justify-center sm:justify-between bg-[#25273c] ${
+              theme == "dark" ? "bg-[#25273c]" : "bg-[#fafafa]"
+            } gap-3 mx-6 sm:mx-0 rounded-sm py-4 sm:py-5 px-5`}
+          >
             <p className="text-sm text-[#4d5066] sm:block hidden">
               5 items left
             </p>
             <span className="flex sm:gap-3 gap-5">
-              <p className="text-sm text-[#0073e6] hover:text-[#c4d4e8] hover:cursor-pointer">
+              <p className="text-sm text-[#0073e6] hover:text-[#484b6a] hover:cursor-pointer">
                 All
               </p>
-              <p className="text-sm text-[#4d5066] hover:text-[#c4d4e8] hover:cursor-pointer">
+              <p
+                className={`text-sm  ${
+                  theme == "dark"
+                    ? "text-[#4d5066]"
+                    : "text-[#9394a5] font-bold"
+                } hover:text-[#484b6a] hover:cursor-pointer`}
+              >
                 Active
               </p>
-              <p className="text-sm text-[#4d5066] hover:text-[#c4d4e8] hover:cursor-pointer">
+              <p
+                className={`text-sm  ${
+                  theme == "dark"
+                    ? "text-[#4d5066]"
+                    : "text-[#9394a5] font-bold"
+                } hover:text-[#484b6a] hover:cursor-pointer`}
+              >
                 Completed
               </p>
             </span>
 
-            <p className="text-sm text-[#4d5066] sm:block hidden hover:text-[#c4d4e8] hover:cursor-pointer">
+            <p className="text-sm text-[#4d5066] sm:block hidden hover:text-[#484b6a] hover:cursor-pointer">
               Clear completed
             </p>
           </div>
         </div>
       </div>
-      <p className="text-center mt-5 sm:py-9 my-3 text-[#4d5066]">
+      <p
+        className={`text-center mt-5 sm:py-9 my-3  ${
+          theme == "dark" ? "text-[#4d5066]" : "text-[#9394a5]"
+        }`}
+      >
         Drag and drop to reorder list
       </p>
     </div>
